6154f9c40589c88a9d15c1afee264b3f5e6bbbd0
nick <nikoloz.danelia@redberry.ge>
2023-07-12 20:05:00 +0400

    fix: showing likes on movies page on quote cards

--- components/movies/ViewQuote/useViewQuote.tsx
+++ components/movies/ViewQuote/useViewQuote.tsx
@@ -1,13 +1,29 @@
-import { deleteQuote } from '@/services';
+import { addComment, deleteQuote } from '@/services';
+import { CommentType, addCommentType } from '@/types';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 export const useViewQuote = (id: number) => {
+  const { register, handleSubmit, reset } = useForm();
+  const [newComments, setNewComments] = useState<CommentType[]>([]);
   const router = useRouter();
   const handleDelete = async () => {
     const res = await deleteQuote(id);
     res.status === 200 && router.reload();
     return res;
   };
+  const onSubmit = async (data: addCommentType) => {
+    try {
+      const response = await addComment(data);
+      setNewComments((prev) => [...[response.data], ...prev]);
+      reset();
+    } catch (e) {}
+  };
   return {
     handleDelete,
+    onSubmit,
+    register,
+    handleSubmit,
+    newComments,
   };
 };
