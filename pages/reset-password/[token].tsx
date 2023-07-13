a7fcf2c21b5b89e46101edd6bc5814441475afc7
nick <nikoloz.danelia@redberry.ge>
2023-07-13 21:44:59 +0400

    feat: translating password reset page

--- pages/reset-password/[token].tsx
+++ pages/reset-password/[token].tsx
@@ -3,38 +3,40 @@ import { Input } from '@/components/shared';
 import { useModal } from '@/hooks';
 import { ErrorMessage } from '@hookform/error-message';
 import { usePasswordReset } from '@/hooks';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { GetServerSidePropsContext } from 'next';
 import React from 'react';
+import { useTranslation } from 'next-i18next';
 const Token = () => {
   const { setOpenModal } = useModal();
   const { handleSubmit, onSubmit, reset, errors, password, router } =
     usePasswordReset();
+  const { t } = useTranslation();
   return (
     <div className='px-2 bg-default sm:px-0 fixed h-screen w-screen flex items-center justify-center z-50'>
       <div className='bg-horizontal-gradient bg-modal relative max-w-xl sm:px-20 sm:py-12 px-8 py-16 flex flex-col justify-center items-center text-white rounded-lg gap-4'>
-        <h2 className='text-4xl'>Create new password</h2>
-        <p className='text-center text-gray-600 w-80'>
-          Your new password must be different from previous used passwords
-        </p>
+        <h2 className='text-4xl text-center'>{t('reset.create_new')}</h2>
+        <p className='text-center text-gray-600 w-80'>{t('reset.different')}</p>
         <form
           onSubmit={handleSubmit(onSubmit)}
           className='w-full flex flex-col gap-7'
         >
           <div className='relative'>
             <Input
-              placeholder='At least 8 & max.15 lower case characters'
-              label='Password'
+              placeholder={t('reset.password_validation')}
+              label={t('reset.password')}
               type='password'
               required={true}
               name='password'
               validation={{
-                required: 'Password is required',
+                required: t('reset.password_required'),
                 minLength: {
                   value: 8,
-                  message: 'Password should be at least 8 characters',
+                  message: t('reset.password_short'),
                 },
                 maxLength: {
                   value: 15,
-                  message: 'Password should not exceede 15 characters',
+                  message: t('reset.password_long'),
                 },
               }}
             />
@@ -44,15 +46,15 @@ const Token = () => {
           </div>
           <div className='relative'>
             <Input
-              placeholder='Confirm password'
-              label='Confirm Password'
+              placeholder={t('reset.confirm_password')}
+              label={t('reset.confirm_password')}
               type='password'
               required={true}
               name='password_confirmation'
               validation={{
-                required: 'Password confirmation is required',
+                required: t('reset.password_confirmation_required'),
                 validate: (value: string) =>
-                  value == password || 'Passwords do not match',
+                  value == password || t('reset.password_mismatch'),
               }}
             />
             <p className='text-red-500 absolute -bottom-5 text-sm'>
@@ -63,7 +65,7 @@ const Token = () => {
             type='submit'
             className='disabled:bg-red-800 w-full text-center py-2 text-base bg-red-600 rounded-4'
           >
-            Reset Password
+            {t('reset.reset')}
           </button>
         </form>
         <div
@@ -75,11 +77,20 @@ const Token = () => {
           className='flex mt-3 items-center gap-2 text-gray-600 cursor-pointer'
         >
           <Back />
-          <p>Back to log in</p>
+          <p>{t('reset.back')}</p>
         </div>
       </div>
     </div>
   );
 };
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { locale = 'en' } = context;
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale)),
+    },
+  };
+}
 
 export default Token;
